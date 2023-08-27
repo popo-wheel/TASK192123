26840d0559d7a5b7a0a3eafe766a0234ad1496f6
Petya <petya200606@gmail.com>
2023-08-27 23:00:27 +0800

    Сделал 23 задание

---
+++ task 23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+void TableCubed(int num)
+{
+    int count = 1;
+  while(count <= num)
+  {
+   Console.WriteLine($"{count, 3} -> {count*count*count, 5}");
+   count++;
+  }
+}
+
+Console.WriteLine("Введите натуральное число ");
+int number = Convert.ToInt32(Console.ReadLine());  
+
+TableCubed(number);
